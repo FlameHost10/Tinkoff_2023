2d3edc65e47cd14e9d2aa6563b21f739b0318fee
Polina Bushlya <pdelo2310@gmail.com>
2023-10-10 22:47:07 +0300

    few changes

--- project-template/src/main/java/edu/hw1/Main.java
+++ project-template/src/main/java/edu/hw1/Main.java
@@ -1,12 +1,7 @@
 package edu.hw1;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 public final class Main {
 
-    private Main() {
-    }
 
     public static void main(String[] args) {
     }
