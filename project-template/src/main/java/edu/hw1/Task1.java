04b4a7cb968c7a684a7e8461c392f85dea829011
Polina Bushlya <pdelo2310@gmail.com>
2023-10-10 22:34:08 +0300

    Task0

---
+++ project-template/src/main/java/edu/hw1/Task1.java
@@ -0,0 +1,7 @@
+package edu.hw1;
+
+public class Task1 {
+    public static int minutesToSeconds(String time){
+        return 0;
+    }
+}
---
+++ project-template/src/test/java/edu/hw1/Test1.java
@@ -0,0 +1,13 @@
+package edu.hw1;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class Test1 {
+    @Test
+    @DisplayName("Длина видео в секундах")
+    void filterEvenNumbers() {
+
+    }
+}
