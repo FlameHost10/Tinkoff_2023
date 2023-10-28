1d4af4fac930c03a1cf5e59c20ed229478a5ac8b
Polina Bushlya <pdelo2310@gmail.com>
2023-10-28 15:06:36 +0300

    Tasks 1, 2, 3, 4, 5, 6

--- project-template/src/main/java/edu/Main.java
+++ project-template/src/main/java/edu/Main.java
@@ -1,10 +1,25 @@
 package edu;
 
+import edu.hw3.Stock;
+import edu.hw3.Task3;
+import edu.hw3.Task4;
+import edu.hw3.Task6;
 import edu.project1.Project1;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Stack;
+import java.util.stream.Stream;
 
 public final class Main {
     public static void main(String[] args) throws IOException {
-        Project1.run();
+        Tre<String, String> treeMap = new
+
     }
 }
---
+++ project-template/src/test/java/edu/hw3/Test6.java
@@ -0,0 +1,12 @@
+package edu.hw3;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class Test6 {
+    @Test
+    @DisplayName("Биржа")
+    void StockExchangeTest{
+        
+    }
+}
