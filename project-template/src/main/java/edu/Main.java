42ef353263ef52925d71b7cbb9d4b25d173fb9cb
Polina Bushlya <pdelo2310@gmail.com>
2023-10-28 22:46:19 +0300

    All ready

--- project-template/src/main/java/edu/Main.java
+++ project-template/src/main/java/edu/Main.java
@@ -1,25 +1,20 @@
 package edu;
 
-import edu.hw3.Stock;
-import edu.hw3.Task3;
-import edu.hw3.Task4;
-import edu.hw3.Task6;
-import edu.project1.Project1;
-import java.io.IOException;
+import edu.hw3.Task8;
+import org.jetbrains.annotations.NotNull;
+import javax.swing.text.html.HTMLDocument;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashMap;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
-import java.util.PriorityQueue;
-import java.util.Queue;
-import java.util.Stack;
+import java.util.Set;
+import java.util.TreeMap;
 import java.util.stream.Stream;
 
 public final class Main {
-    public static void main(String[] args) throws IOException {
-        Tre<String, String> treeMap = new
-
+    public static void main(String[] args) {
     }
+
 }
--- project-template/src/test/java/edu/hw3/Test6.java
+++ project-template/src/test/java/edu/hw3/Test6.java
@@ -6,7 +6,23 @@ import org.junit.jupiter.api.Test;
 public class Test6 {
     @Test
     @DisplayName("Биржа")
-    void StockExchangeTest{
-        
+    void StockExchangeTest(){
+        Task6.StockExchange stockExchange = new Task6.StockExchange();
+        Stock tinkoff = new Stock("tinkoff", 5.66),
+            sber = new Stock("sber", 8.31),
+            vtb = new Stock("vtb", 4.78);
+
+        stockExchange.add(tinkoff);
+        stockExchange.add(sber);
+        System.out.println(stockExchange + " -> " + stockExchange.mostValuableStock());
+
+        stockExchange.remove(sber);
+        System.out.println(stockExchange + " -> " + stockExchange.mostValuableStock());
+
+        stockExchange.add(vtb);
+        System.out.println(stockExchange + " -> " + stockExchange.mostValuableStock());
+
+
+
     }
 }
